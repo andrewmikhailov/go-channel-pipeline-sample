f9336f2e994050bddada8e393b0b89fae4093824
Vitche Research Team Developer <developer@vitche.com>
2017-10-27 15:26:15 +0000

    1. Passing data from the first channel to the second channel and then to the third channel.

--- main.go
+++ main.go
@@ -4,11 +4,51 @@ import (
 	"fmt"
 )
 
+func Pipeline(from chan string, to chan string, n int) {
+	i := 0
+	for i < n {
+		select {
+		case value := <-from:
+			to <- value
+			i = i + 1
+		}
+	}
+}
+
 func main() {
+
+	// Empty channels
 	first := make(chan string)
 	second := make(chan string)
 	third := make(chan string)
-	fmt.Printf("\nFirst channel:\n", first, "\n")
-	fmt.Printf("\nSecond channel:\n", second, "\n")
-	fmt.Printf("\nThird channel:\n", third, "\n")
+
+	// Write to the first channel
+	go func() {
+		first <- "First value"
+	}()
+	go func() {
+		first <- "Second value"
+	}()
+	go func() {
+		first <- "Third value"
+	}()
+
+	// Pass data from the first channel into the second channel and
+	// from the second channel into the third channel.
+	go func() {
+		Pipeline(first, second, 3)
+	}()
+	go func() {
+		Pipeline(second, third, 3)
+	}()
+
+	// Render data from the third channel
+	i := 0
+	for i < 3 {
+		select {
+		case value := <-third:
+			fmt.Printf(value + "\n")
+			i = i + 1
+		}
+	}
 }
